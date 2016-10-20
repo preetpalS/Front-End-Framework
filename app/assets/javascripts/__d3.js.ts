e70863a14ee49a527804d9e94cac050a564a4773
Preetpal Sohal <preetpal.sohal@gmail.com>
2016-10-20 00:21:35 -0700

    Update __d3.js.ts
    Removed comment.

--- app/assets/javascripts/__d3.js.ts
+++ app/assets/javascripts/__d3.js.ts
@@ -1,6 +1,1 @@
 /// <reference path="type_definitions/d3/d3.d.ts"/>
-
-
-// Reference to global JQuery (this file must be in root of app/assets/javascripts directory to enable
-// the TypeScript compiler to be able to compile the library even without it being processed by the
-// TypeScript-Sprockets preprocessor).
