28ca1d441f40bf5ee8e8b46af53772d927e532f6
Preetpal Sohal <preetpal.sohal@gmail.com>
2019-08-06 16:43:04 -0700

    tslint related changes

--- javascripts/enumerations/object_life_cycle.ts
+++ javascripts/enumerations/object_life_cycle.ts
@@ -3,5 +3,5 @@
 export const enum ObjectLifeCycle {
     Transient = 0, // Only for single page, object should automatically be destroyed when navigating from page
     VariablePersistence = 1, // Lifetime is managed manually (should not be automatically destroyed when navigating pages)
-    InfinitePersistence = 2 // Not to be destroyed (intended to be persistent across page navigation)
-};
+    InfinitePersistence = 2, // Not to be destroyed (intended to be persistent across page navigation)
+}
